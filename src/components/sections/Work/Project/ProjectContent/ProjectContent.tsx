02c35c3af07be081a1ce8ff308f7e44c6b9bf277
Osama_Salem <os.salem94@gmail.com>
2021-03-05 21:55:11 +0200

    feat: hide link icon if there's no link in projects

--- src/components/sections/Work/Project/ProjectContent/ProjectContent.tsx
+++ src/components/sections/Work/Project/ProjectContent/ProjectContent.tsx
@@ -1,43 +1,50 @@
-import React from 'react'
-import { IProjectContent } from '../../../../../@types/Work'
-import classes from './ProjectContent.module.css'
-import IconLink from '../../../../IconLink/IconLink'
-import MonoTextList from '../../../../MonoTextList/MonoTextList'
+import React from "react";
+import { IProjectContent } from "../../../../../@types/Work";
+import classes from "./ProjectContent.module.css";
+import IconLink from "../../../../IconLink/IconLink";
+import MonoTextList from "../../../../MonoTextList/MonoTextList";
 
 interface Props {
-    projectContentClassName: string,
-    projectTechListClassName: string,
-    projectLinksClassName: string,
-    projectContentData: IProjectContent
+  projectContentClassName: string;
+  projectTechListClassName: string;
+  projectLinksClassName: string;
+  projectContentData: IProjectContent;
 }
 
-const ProjectContent: React.FC<Props> = props => {
-    return (
-        <div className={`${props.projectContentClassName} ${classes.ProjectContent}`}>
-            <p className={classes.ProjectOverline}>
-                {props.projectContentData.overline}
-            </p>
-            <h3 className={classes.ProjectTitle}>
-                {props.projectContentData.title}
-            </h3>
-            <p className={classes.ProjectDescription}>
-                {props.projectContentData.description}
-            </p>
-            <MonoTextList data={props.projectContentData.techList} className={props.projectTechListClassName} />
-            <div className={`${props.projectLinksClassName} ${classes.ProjectLinks}`}>
-                <IconLink
-                    aria-label={`${props.projectContentData.title} github repo`}
-                    href={props.projectContentData.projectLinks.github}
-                    iconType="Github"
-                />
-                <IconLink
-                    aria-label={`${props.projectContentData.title} live link`}
-                    href={props.projectContentData.projectLinks.live}
-                    iconType="External"
-                />
-            </div>
-        </div>
-    )
-}
+const ProjectContent: React.FC<Props> = (props) => {
+  return (
+    <div
+      className={`${props.projectContentClassName} ${classes.ProjectContent}`}
+    >
+      <p className={classes.ProjectOverline}>
+        {props.projectContentData.overline}
+      </p>
+      <h3 className={classes.ProjectTitle}>{props.projectContentData.title}</h3>
+      <p className={classes.ProjectDescription}>
+        {props.projectContentData.description}
+      </p>
+      <MonoTextList
+        data={props.projectContentData.techList}
+        className={props.projectTechListClassName}
+      />
+      <div className={`${props.projectLinksClassName} ${classes.ProjectLinks}`}>
+        {props.projectContentData.projectLinks.github && (
+          <IconLink
+            aria-label={`${props.projectContentData.title} github repo`}
+            href={props.projectContentData.projectLinks.github}
+            iconType="Github"
+          />
+        )}
+        {props.projectContentData.projectLinks.live && (
+          <IconLink
+            aria-label={`${props.projectContentData.title} live link`}
+            href={props.projectContentData.projectLinks.live}
+            iconType="External"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default ProjectContent
+export default ProjectContent;
